1c7081d219fa3c6f6c7efeb5294c02e4cc9837ab
Dmitriy770 <zdima08@gmail.com>
2023-03-21 14:12:16 +0300

    describe part of the commands VoiceManagerModule

---
+++ src/DiscordBot.Api/Modules/VoiceManagerModule.cs
@@ -0,0 +1,33 @@
+﻿using Discord.Interactions;
+
+namespace DiscordBot.Api.Modules;
+
+[Group("voice", "Commands for managing the voice channel")]
+public class VoiceManagerModule : InteractionModuleBase<SocketInteractionContext>
+{
+    [SlashCommand("claim", "Command to capture a voice channel")]
+    public async Task ClaimVoice()
+    {
+        await RespondAsync("Claim", ephemeral: true);
+    }
+
+    [Group("set", "Setting the parameters of the voice channel")]
+    public class SubVoiceManagerCommand : InteractionModuleBase<SocketInteractionContext>
+    {
+        [SlashCommand("limit", "Sets the voice channel limit")]
+        public async Task SetVoiceLimit(
+            [Summary("limit", "Maximum number of users"), MinValue(1), MaxValue(99)]
+            int limit)
+        {
+            await RespondAsync($"New voice limit: {limit}", ephemeral: true);
+        }
+        
+        [SlashCommand("name", "Sets the voice channel name")]
+        public async Task SetVoiceName(
+            [Summary("name", "Voice channel names"), MinLength(1), MaxValue(20)]
+            string name)
+        {
+            await RespondAsync($"New voice name: {name}", ephemeral: true);
+        }
+    }
+}
