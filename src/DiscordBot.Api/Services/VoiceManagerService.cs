1be40faca8a00cca14d120f85791047f702f08df
Dmitriy770 <zdima08@gmail.com>
2023-04-03 14:49:41 +0300

    fix SlashCommandBuilder in VoiceManagerService

--- src/DiscordBot.Api/Services/VoiceManagerService.cs
+++ src/DiscordBot.Api/Services/VoiceManagerService.cs
@@ -31,35 +31,40 @@ public sealed class VoiceManagerService
             _createVoiceChannel = _client.GetChannel(options.CurrentValue.CreateVoiceId) as SocketVoiceChannel;
 
             var guildCommand = new SlashCommandBuilder()
-                .WithDMPermission(false)
                 .WithName("voice")
                 .WithDescription("Commands for managing the voice channel")
                 .AddOption(new SlashCommandOptionBuilder()
                     .WithName("claim")
-                    .WithDescription("Command to capture a voice channel"))
+                    .WithDescription("Command to capture a voice channel")
+                    .WithType(ApplicationCommandOptionType.SubCommand))
                 .AddOption(new SlashCommandOptionBuilder()
                     .WithName("set")
                     .WithDescription("Setting the parameters of the voice channel")
+                    .WithType(ApplicationCommandOptionType.SubCommandGroup)
                     .AddOption(new SlashCommandOptionBuilder()
                         .WithName("limit")
                         .WithDescription("Sets the voice channel limit")
+                        .WithType(ApplicationCommandOptionType.SubCommand)
                         .AddOption("limit", ApplicationCommandOptionType.Integer, "Maximum number of users",
                             isRequired: true, minValue: 1, maxValue: 99))
                     .AddOption(new SlashCommandOptionBuilder()
                         .WithName("name")
                         .WithDescription("Sets the voice channel name")
+                        .WithType(ApplicationCommandOptionType.SubCommand)
                         .AddOption("name", ApplicationCommandOptionType.String, "Voice channel name",
                             isRequired: true, minLength: 1, maxLength: 20)))
                 .AddOption(new SlashCommandOptionBuilder()
                     .WithName("reset")
                     .WithDescription("Resets voice channel parameters")
+                    .WithType(ApplicationCommandOptionType.SubCommandGroup)
                     .AddOption(new SlashCommandOptionBuilder()
                         .WithName("limit")
-                        .WithDescription("Resets voice channel limit"))
+                        .WithDescription("Resets voice channel limit")
+                        .WithType(ApplicationCommandOptionType.SubCommand))
                     .AddOption(new SlashCommandOptionBuilder()
                         .WithName("name")
-                        .WithDescription("Resets voice channel name")));
-
+                        .WithDescription("Resets voice channel name")
+                        .WithType(ApplicationCommandOptionType.SubCommand)));
             try
             {
                 await _client.Rest.CreateGuildCommand(guildCommand.Build(), _guild.Id);
