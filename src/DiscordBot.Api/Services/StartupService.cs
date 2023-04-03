0abc91147ddde6136a87dc47a3ebf15fb46d2685
Dmitriy770 <zdima08@gmail.com>
2023-04-03 14:41:28 +0300

    remove Modules

--- src/DiscordBot.Api/Services/StartupService.cs
+++ src/DiscordBot.Api/Services/StartupService.cs
@@ -1,6 +1,4 @@
-﻿using System.Reflection;
-using Discord;
-using Discord.Interactions;
+﻿using Discord;
 using Discord.WebSocket;
 
 namespace DiscordBot.Api.Services;
@@ -9,34 +7,17 @@ public sealed class StartupService
 {
     private readonly DiscordSocketClient _client;
     private readonly IConfiguration _configuration;
-    private readonly IServiceProvider _provider;
 
-    public StartupService(DiscordSocketClient client, IConfiguration configuration, IServiceProvider provider)
+    public StartupService(DiscordSocketClient client, IConfiguration configuration)
     {
         _client = client;
         _configuration = configuration;
-        _provider = provider;
     }
 
     public async Task RunAsync()
     {
         await _client.LoginAsync(TokenType.Bot, _configuration["Token"]);
         await _client.StartAsync();
-        
-        // TODO убрать обработчик команд для модуля
-        // _client.Ready += async () =>
-        // {
-        //     var interactionService = new InteractionService(_client);
-        //
-        //     await interactionService.AddModulesAsync(Assembly.GetEntryAssembly(), _provider);
-        //     await interactionService.RegisterCommandsToGuildAsync(_configuration.GetValue<ulong>("Guild:Id"));
-        //
-        //     _client.InteractionCreated += async interaction =>
-        //     {
-        //         var ctx = new SocketInteractionContext(_client, interaction);
-        //         await interactionService.ExecuteCommandAsync(ctx, _provider);
-        //     };
-        // };
     }
     
 }
